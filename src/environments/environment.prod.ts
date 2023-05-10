aab2cf95a239015b6439787c79956f6118aec098
Edwin Tumax <edwintumax@kinal.edu.gt>
2023-05-09 19:19:22 -0600

    Agregando servicio de carreras tecnicas

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    base_url: "http://localhost:5064/kalum-management/v1"
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    base_url: "https://localhost:7171/kalum-management/v1"
+}
